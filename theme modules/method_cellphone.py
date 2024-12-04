de1903901c9faa5f73a60efa91d550758c0bb043
Naikst <kuzmenko225@gmail.com>
2024-12-04 07:37:18 +0300

    Классы и ООП

--- theme modules/method_cellphone.py
+++ theme modules/method_cellphone.py
@@ -1,36 +1,24 @@
-# Класс CellPhone содержит данные о сотовом телефоне
 
+# Предположим, что ret_tecno() должен возвращать название бренда
+# Класс CellPhone представляет информацию о мобильном телефоне
 class CellPhone:
+    def __init__(self, manufacturer, model_number, retail_price):
+        # Конструктор создает объект мобильного телефона
+        # manufacturer - производитель телефона
+        # model_number - номер модели телефона
+        # retail_price - розничная цена телефона
+        self.__manufacturer = manufacturer
+        self.__model_number = model_number
+        self.__retail_price = retail_price
 
-    # метод __init__ инициализирует атрибуты модели, производителя и цены
-    def __init__(self, tecno, model_type, price):
-        self.__model = model_type
-        self.__tecno = tecno  # Исправлено имя атрибута
-        self.__price = price
+    def ret_tecno(self):
+        # Метод возвращает название производителя (бренда) телефона
+        return self.__manufacturer
 
-    # метод set_model_type принимает аргумент для
-    # типа модели телефона
-    def set_model_type(self, model_type):
-        self.__model = model_type
+    def get_model_number(self):
+        # Метод возвращает номер модели телефона
+        return self.__model_number
 
-    # метод ret_tecno принимает аргумент для
-    # производителя телефона
-    def ret_tecno(self, tecno):
-        self.__tecno = tecno
-
-    # Метод retail_price принимает аргумент для
-    # розничной цены телефона
-    def retail_price(self, price):
-        self.__price = price
-
-    # метод get_model_type возвращает тип модели телефона
-    def get_model_type(self):
-        return self.__model
-
-    # метод get_tecno возвращает производителя телефона
-    def get_tecno(self):
-        return self.__tecno
-
-    # метод get_retail_price возвращает розничную цену телефона
     def get_retail_price(self):
-        return self.__price
+        # Метод возвращает розничную цену телефона
+        return self.__retail_price
