6d055fa8917e4d03858a279d2d5a03c95ee36a03
Naikst <kuzmenko225@gmail.com>
2024-12-22 12:27:53 +0300

    Задача "Файлы в операционной системе"

--- module_7/module_7_5.py
+++ module_7/module_7_5.py
@@ -2,7 +2,7 @@ import os  # для работы с файловой системой.
 import time  # для форматирования времени.
 
 # Указываем директорию для обхода
-directory = r"K:\Practical_work\new-feature" # Проход по всем файлам и папкам в указанной директории
+directory = r"C:\Practical_work\Practices" # Проход по всем файлам и папкам в указанной директории
 
 for root, dirs, files in os.walk(directory):
     # Функция os.walk() позволяет рекурсивно обходить все файлы и папки в указанной директории. Она возвращает кортеж из трех элементов
@@ -10,9 +10,12 @@ for root, dirs, files in os.walk(directory):
     for file in files:
         filepath = os.path.join(root, file)
         filetime = os.path.getmtime(filepath)  # преобразует время из формата UNIX в локальное время.
+
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))  # форматирует дату в привычный вид "день.месяц.год часы:минуты".
         filesize = os.path.getsize(filepath)  # возвращает размер файла в байтах.
-        relative_path = os.path.relpath(filepath, directory)
+        parent = os.path.dirname(filepath)  # возвращает путь до родительской директории.
+        relative_path = os.path.relpath(filepath, directory) # возвращает относительный путь к файлу.
+
         print(f'Обнаружен файл: {file}, Путь: .\\{relative_path}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория:.')
 
 
