df11c6dace4560b0c4dfa3a98da7f9567167d972
Naikst <kuzmenko225@gmail.com>
2025-01-02 14:53:02 +0300

    Задача "Вызов разом"

--- Module_9/module_9_1.py
+++ Module_9/module_9_1.py
@@ -1,13 +1,10 @@
 def apply_all_func(int_list, *functions):
-    results = {}
-    for func in functions:
-        results[func.__name__] = func(int_list)
-    return results
+    results = {}  # Создаем пустой словарь для хранения результатов
+    for func in functions:  # Перебираем все переданные функции
+        results[func.__name__] = func(int_list)  # Применяем функцию к списку и записываем результат в словарь
+    return results  # Возвращаем словарь с результатами
 
 
 # Пример выполнения программы:
 print(apply_all_func([6, 20, 15, 9], max, min))  # Вывод: {'max': 20, 'min': 6}
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))  # Вывод: {'len': 4, 'sum': 50, 'sorted': [6, 9, 15, 20]}
-
-
-
